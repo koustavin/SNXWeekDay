96698b724ae8c07add5adb2ddacae74d3fc44d10
koustavin <123743596+koustavin@users.noreply.github.com>
2023-02-20 01:22:10 +0530

    Delete SNX_WeekDay.py

--- SNX_WeekDay.py
+++
@@ -1,9 +0,0 @@
-from pandas_datareader import data
-import datetime as dt
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-SNX = data.DataReader('^SNX', 'stooq', startTime, endTime)
-print(SNX.info())
